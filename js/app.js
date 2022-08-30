ca85504363c5c06f736196e6a55b0e41cb61c5bf
Rodolfo088 <106852634+Rodolfo088@users.noreply.github.com>
2022-08-29 21:54:28 -0300

    Incorporar Arrays y Primera entrega del Proyecto Final

--- js/app.js
+++ js/app.js
@@ -1,63 +1,72 @@
+class paciente {
+    constructor(nombre, apellido, edad, email, telefono, direccion, motivodeconsulta, id){
+this.nombre = nombre;
+this.apellido = apellido;
+this.edad = edad;
+this.email = email;
+this.telefono = telefono;
+this.direccion = direccion;
+this.motivodeconsulta = motivodeconsulta;
+this.id = id;
 
-let Ingresaunaopciondecompra = prompt (" Seleccione un producto \n[1]Zapato \n[2]Pantalon \n[3]Remera \n[4]Campera");
-switch (Ingresaunaopciondecompra) {
-    case "1":
-        let precio = 25000 ;
-        let descuento = 0.20 ;
-        let iva = 1.21 ; 
-        let totaldescuento = precio* descuento ;
-        let subtotal =precio - totaldescuento ;
-        let totaliva = subtotal - (subtotal / iva) ;
-        let total = subtotal * iva ;
-        alert("precio pantalo :" +precio+ "\n"+
-        "descuento :" +totaldescuento+ "\n"+
-        "monto iva :" +totaliva+ "\n"+
-        "Total a paga :" +total ) ;
-    break;
-   
-    case "2":
-        let precio2 = 18000 ;
-        let descuento2 = 0.20 ;
-        let iva2 = 1.21 ; 
-        let totaldescuento2 = precio2* descuento2 ;
-        let subtotal2 =precio2 - totaldescuento2 ;
-        let totaliva2 = subtotal2 - (subtotal2 / iva2) ;
-        let total2 = subtotal2 * iva2 ;
-        alert("precio pantalo :" +precio2+ "\n"+
-        "descuento :" +totaldescuento2+ "\n"+
-        "monto iva :" +totaliva2+ "\n"+
-        "Total a paga :" +total2 ) ;
-    break;
+    }
+}
 
-    case "3":
-        let precio3 = 7500 ;
-        let descuento3 = 0.20 ;
-        let iva3 = 1.21 ; 
-        let totaldescuento3 = precio3* descuento3 ;
-        let subtotal3 =precio3 - totaldescuento3 ;
-        let totaliva3 = subtotal3 - (subtotal3 / iva3) ;
-        let total3 = subtotal3 * iva3 ;
-        alert("precio pantalo :" +precio3+ "\n"+
-        "descuento :" +totaldescuento3+ "\n"+
-        "monto iva :" +totaliva3+ "\n"+
-        "Total a paga :" +total3 ) ;
-    break;
+const pacientes = [];
+let option = 0;
+do {
+    option = parseInt(
+        prompt("ingrese \n 1 para agregar paciente \n 2 para ver todos los pacientes \n 3 consultar paciente \n 8 para salir")
+        );
+    switch (option) {
+        case 1: 
+        agregarPaciente(pacientes);
+        break;
+        case 2: 
+        verTodosLosPacientes(pacientes);
+        break;
+        case 3:
+        consultarPacientes(pacientes);
+        break;
+        default:
+        alert ("opcion incorrecta");
+        break;
+    }
+}  while (option != 8);
 
-    case "4":
-        let precio4 = 33000 ;
-        let descuento4 = 0.20 ;
-        let iva4 = 1.21 ; 
-        let totaldescuento4 = precio4* descuento4 ;
-        let subtotal4 =precio4 - totaldescuento4 ;
-        let totaliva4 = subtotal4 - (subtotal4 / iva4) ;
-        let total4 = subtotal4 * iva4 ;
-        alert("precio pantalo :" +precio4+ "\n"+
-        "descuento :" +totaldescuento4+ "\n"+
-        "monto iva :" +totaliva4+ "\n"+
-        "Total a paga :" +total4 ) ;
-    break;
 
-    default:
-    alert ("Error Opcion Invalidad");
-    break;
+
+//agregar
+
+function agregarPaciente(pacientes) {
+let nombre = prompt("ingrese nombre");
+let apellido = prompt("ingrese apellido");
+let edad = parseInt(prompt("ingrese edad"));
+let email = prompt("ingrese email");
+let telefono = parseInt(prompt("ingrese telefono"));
+let direccion = prompt("ingrese direccion");
+let motivodeconsulta = prompt("ingrese motivo de consulta: \n - blanqueamiento \n - ortodoncia \n - control general \n - extraccion");
+let newUser = new paciente(nombre, apellido, edad, email, telefono, direccion, motivodeconsulta, pacientes.length + 1);
+pacientes.push(newUser);
+console.log("se agrego usuario", newUser);
+}
+
+//ver todos
+function verTodosLosPacientes(pacientes) {
+    for (const paciente of pacientes) {
+    console.log(paciente);
+   }
+}
+
+// ver paciente 
+function consultarPacientes(pacientes) {
+    let texto = "";
+    for (let index = 0; index < pacientes.length; index++) {
+        texto = texto.concat(
+        `Ingrese ${index + 1} para el paciente ${pacientes[index].nombre} \n`
+        );
+    }
+    console.log(texto);
+    const option = parseInt(prompt(texto));
+    console.log(pacientes[option - 1]);
 }
